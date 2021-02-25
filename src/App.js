159544742d073abe0503e0a67685274a29c2b5e5
mcoulier <michielcoulier@gmail.com>
2021-02-25 18:09:35 +0100

    initial upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const url = "https://api.mixcloud.com/discover/funk/popular/";
+  const [popular, setPopular] = useState([])
+
+  useEffect(() => {
+    async function fetchUrl() {
+      try {
+        let response = await fetch(url);
+        response = await response.json();
+        console.log(response.data);
+        setPopular(response.data)
+      } catch (err) {
+        alert(err);
+      }
+    }
+    fetchUrl();
+  }, []);
+
+  return <div className="App">
+    {
+      popular.map((mix, mixIndex) => (
+        <li key={mixIndex}>{mix.name}</li>
+      ))
+    }
+  </div>;
 }
 
 export default App;
